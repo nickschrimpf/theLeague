ce2ebb62f8f28804c4076fb64cd5f7e5054d46d8
Nicholas Schrimpf <nickschrimpf@Nicholass-MacBook-Pro.local>
2021-12-17 19:16:44 -0700

    added AngularMaterial and a start on nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialsModule } from './materials.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
@@ -11,6 +11,9 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './navigation/sidenav/sidenav.component';
+import { HeaderComponent } from './navigation/header/header.component';
 
 
 @NgModule({
@@ -19,14 +22,18 @@ import { provideStorage,getStorage } from '@angular/fire/storage';
     WelcomePageComponent,
     LoginComponent,
     SignUpComponent,
+    SidenavComponent,
+    HeaderComponent,
   ],
   imports: [
+    MaterialsModule,
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
+    BrowserAnimationsModule,
 
   ],
   providers: [],
---
+++ src/app/materials.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+    declarations:[],
+    imports:[
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule
+    ],
+    exports:[
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule
+    ],
+
+})
+export class MaterialsModule{}
